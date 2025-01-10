ce51a73011a81f7e3ec01e82e9094ff57209d1db
Huskarnov <youcef.merad.batna2m@gmail.com>
2025-01-10 16:36:13 +0100

    tile finder done

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const findTileButton = document.querySelector('.tileFinder button');
-const tileX = document.querySelector('.coordinates input:nth-child(1)')
-const tileY = document.querySelector('.coordinates input:nth-child(2)')
 const tile = document.querySelector('.tile');
 
+
+findTileButton.addEventListener('click',(e)=>{
+    e.preventDefault()
+    const tileX = document.querySelector('#xCoord')
+    const tileY = document.querySelector('#yCoord')
+
+    tile.style.gridColumn  = `${tileX.value - 5}`;
+    tile.style.gridRow  = `${tileY.value - 80}`;
+    tile.style.display = 'block';
+    console.log('xxx');
+});
