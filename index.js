356a0058757ad28a957c9da1ccc779f0b79a6d94
Huskarnov <youcef.merad.batna2m@gmail.com>
2025-01-10 16:19:26 +0100

    logic initialization

---
+++ index.js
@@ -0,0 +1,5 @@
+const findTileButton = document.querySelector('.tileFinder button');
+const tileX = document.querySelector('.coordinates input:nth-child(1)')
+const tileY = document.querySelector('.coordinates input:nth-child(2)')
+const tile = document.querySelector('.tile');
+
