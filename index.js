935101588eccf641aa1747445807cbde99685fd3
Huskarnov <youcef.merad.batna2m@gmail.com>
2025-01-11 14:51:04 +0100

    added metal data + generator

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const metals = [
-    [[]],
-    [],
-    [],
-    [],
-    []
+    [[279,307],[223,191],[269,277],[231,166],[203,269],[190,138],[308,217]],
+    [[179,191],[264,312],[285,340],[245,217],[251,142],[211,201],[302,177]],
+    [[208,271],[285,226],[255,213]],
+    [[202,260]]
 ];
-// [],[],[],[],[],[],[],[],[],[],[],
+// [],[],[],[],[],[],[]
 
 const quests = [
-    [[313,186], [305, 177],[288,192],[303, 191],[312, 180]],
+    [[313,186],[305,177],[288,192],[303,191],[312,180]],
     [[294,193],[297,198],[277,201],[282,205],[290,212]],
     [[274,198],[282,168],[283,210],[250,197],[284,227]],
     [[208,103],[267,209],[263,245],[307,233],[278,287],[274,169]],
@@ -32,8 +31,9 @@ const inputY = document.querySelector('#yCoord');
 const inputWrapperX = document.querySelector('.inputWrapper:nth-child(1)');
 const inputWrapperY = document.querySelector('.inputWrapper:nth-child(2)');
 
-const questButtonsWrapper = document.querySelector('.lowBarSection:nth-child(1) > div');
-const metalButtonsWrapper = document.querySelector('.lowBarSection:nth-child(2) > div');
+const questButtonsWrapper = document.querySelector('.lowBarSection:nth-child(1) div');
+const metalButtonsWrapper = document.querySelector('.lowBarSection:nth-child(2) div');
+
 
 // ////////////////////GET TILE COORDS////////////////////////////
 mapGrid.addEventListener('click', (event) =>{
@@ -124,6 +124,34 @@ for (let i = 0; i <= 8; i++){
                 questMarker.classList.add('glowy');
                 questMarker.style.display = 'block';
                 mapGrid.appendChild(questMarker);
+
+                tile.style.display ='none';
+                
+            };
+
+        }
+    );
+};
+// //////////////////////////////////////////////////////////
+
+for (let g = 0; g <= 3; g++){
+    metalButtonsWrapper.children[g].addEventListener('click', (event) => {
+
+        clearGrid();
+        
+
+        for( let y = metals[g].length - 1; y >=0; y--){
+        
+                const gridXcoord = (metals[g])[y][0] - 5;
+                const gridYcoord = (metals[g])[y][1] - 80;
+
+                const metalMarker = document.createElement('div');
+                metalMarker.style.gridColumn = `${gridXcoord - 1}`;
+                metalMarker.style.gridRow = `${gridYcoord}`;
+                metalMarker.classList.add('tile');
+                metalMarker.classList.add('glowy');
+                metalMarker.style.display = 'block';
+                mapGrid.appendChild(metalMarker);
 
                 tile.style.display ='none';
                 
